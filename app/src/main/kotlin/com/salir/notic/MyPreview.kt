8c4f4c2e2ec39d203f6476bf9b40290d0b532110
Oleg <Sal1r57@yandex.ru>
2025-06-03 13:49:50 +0300

    feat: added "Clear focus" button to preview

--- app/src/main/kotlin/com/salir/notic/MyPreview.kt
+++ app/src/main/kotlin/com/salir/notic/MyPreview.kt
@@ -3,9 +3,12 @@ package com.salir.notic
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.salir.myui.components.Button
@@ -23,6 +27,8 @@ import com.salir.myui.theme.Theme
 
 @Composable
 fun MyPreview() {
+    val focusManager = LocalFocusManager.current
+
     var name by remember { mutableStateOf("") }
     var applyedName by remember { mutableStateOf("") }
 
@@ -42,17 +48,32 @@ fun MyPreview() {
             modifier = Modifier.fillMaxWidth()
         )
 
-        Button(
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .fillMaxWidth(0.6f),
-            onClick = {
-                applyedName = name
-            }
+        Row(
+            modifier = Modifier.padding(top = 8.dp)
         ) {
-            Text(
-                text = "Apply name"
-            )
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    applyedName = name
+                }
+            ) {
+                Text(
+                    text = "Apply name"
+                )
+            }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    focusManager.clearFocus()
+                }
+            ) {
+                Text(
+                    text = "Clear focus"
+                )
+            }
         }
     }
 }
